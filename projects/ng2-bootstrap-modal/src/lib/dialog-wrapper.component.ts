ef7877acf02c3636d57685e880167c8a4b55a02a
akkaradej <akkaradej14@gmail.com>
2020-02-07 23:31:17 +0700

    rebuild with angular native library from cli, bring entryComponents back

---
+++ projects/ng2-bootstrap-modal/src/lib/dialog-wrapper.component.ts
@@ -0,0 +1,70 @@
+import {
+  Component, ViewContainerRef, ViewChild, ComponentFactoryResolver, ReflectiveInjector, Type, Output, EventEmitter
+} from '@angular/core';
+import { DialogComponent } from './dialog.component';
+
+@Component({
+  selector: 'dialog-wrapper',
+  template: `
+    <div #container class="modal fade" style="display:block !important;" role="dialog">
+        <ng-template #element></ng-template>
+    </div>
+`
+})
+export class DialogWrapperComponent {
+
+  @Output() dialogClose = new EventEmitter();
+
+  /**
+   * Target element to insert dialog content component
+   */
+  @ViewChild('element', { read: ViewContainerRef }) public element: ViewContainerRef;
+
+  /**
+   * Link container DOM element
+   */
+  @ViewChild('container') public container;
+
+  /**
+   * Dialog content componet
+   * @type {DialogComponent}
+   */
+  private content: DialogComponent<any, any>;
+
+  /**
+   * Constructor
+   * @param {ComponentFactoryResolver} resolver
+   * @param {DialogService} dialogService
+   */
+  constructor(private resolver: ComponentFactoryResolver) { }
+
+  /**
+   * Adds content dialog component to wrapper
+   * @param {Type<DialogComponent>} component
+   * @return {DialogComponent}
+   */
+  addComponent<T, T1>(component: Type<DialogComponent<T, T1>>) {
+    const factory = this.resolver.resolveComponentFactory(component);
+    const injector = ReflectiveInjector.fromResolvedProviders([], this.element.injector);
+    const componentRef = factory.create(injector);
+    this.element.insert(componentRef.hostView);
+    this.content = componentRef.instance as DialogComponent<T, T1>;
+    this.content.wrapper = this;
+    return this.content;
+  }
+
+  /**
+   * Registers event handler to close dialog by click on backdrop
+   */
+  closeByClickOutside() {
+    const containerEl = this.container.nativeElement;
+    containerEl.querySelector('.modal-content').addEventListener('click', (event) => {
+      event.stopPropagation();
+    });
+    containerEl.addEventListener('click', () => {
+      this.dialogClose.emit();
+    }, false);
+  }
+}
+
+
---
+++ projects/ng2-bootstrap-modal/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng2-bootstrap-modal
+ */
+
+export * from './lib/bootstrap-modal.module';
+export * from './lib/dialog.component';
+export * from './lib/dialog.service';
